8b5fa797359a6668719032cca47301c0b2cf37f7
emenel <mattnl@gmail.com>
2009-01-07 18:32:17 -0500

    removing old code

--- app/controllers/posting.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'twitter')
-config = YAML::load(open(ENV['HOME'] + '/.twitter'))
- 
-twitter = Twitter::Base.new(config['email'], config['password'])
-puts twitter.post("Testing out an #svc post").inspect
- 
-# sending a direct message
-# puts twitter.d('josephdee', 'Testing out an #svc post').inspect
