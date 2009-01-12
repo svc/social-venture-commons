b58292634e94e2af2e0f1e2a6b78be8328ff8e03
emenel <mattnl@gmail.com>
2009-01-12 16:24:07 -0500

    more work on wireframing basic template

--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+	map.root :controller => "ventures", :action => "index"
 end
