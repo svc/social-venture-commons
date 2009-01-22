4f7c42649aa1ad702bf9f95933ed4a8e17c10875
Dan Williams <dan.williams@itthugs.com>
2009-01-22 16:49:56 -0500

    New version of the parser.  Eased up on the rules

--- lib/message_parser.rb
+++ lib/message_parser.rb
@@ -2,23 +2,27 @@ class MessageParser
   def self.parse(tweet)
 
     tweet.text.downcase!
-    puts '------------------------------------'
-    puts " RAW TWEET: #{tweet.text}"
+    
+    puts "------------------------------------"
+    puts "Raw Tweet #{tweet.text}"
     
     return false unless tweet.text =~ /\^/
+
     
-    possible_venture_tags       = tweet.text.scan(/(\^[a-z0-9]*)/).uniq
+    venture_tags       = tweet.text.scan(/(\^[a-z0-9]+)/).uniq
     
-    puts "Possible Venture Tags: #{possible_venture_tags.inspect}"
-    
-    ventures = Venture.all(:conditions=>"tag IN ('#{possible_venture_tags.join('\',\'')}')")
+    ventures = venture_tags.collect do |vt|
+      Venture.find_or_create_by_tag(vt)
+    end
     
     if ventures.any?
-      possible_venture_need_tags  = tweet.text.scan(/\^[a-z0-9]* ![a-z0-9]*/).uniq
+      need_tags  = tweet.text.scan(/![a-z0-9]+/).uniq
       
-      puts "Possible venture Need tags #{possible_venture_need_tags}"
+      puts "Needs : #{need_tags.inspect}"
       
-      needs = Need.all(:conditions=>"tag IN ('#{possible_venture_need_tags.join('\',\'')}')")
+      needs = need_tags.collect do |nt|
+        Need.find_or_create_by_tag(nt)
+      end
       
       account = Account.find_by_twitter_id(tweet.user.id)
       account = Account.create!(:twitter_id=>tweet.user.id,:name=>tweet.user.name,:screen_name=>tweet.user.screen_name,:url=>tweet.user.url,:description=>tweet.user.description,:profile_image_url=> tweet.user.profile_image_url,:account_type => AccountType.find_by_name('User')) unless account      
@@ -31,16 +35,17 @@ class MessageParser
         message.save
       rescue Exception => e
         puts "Duplicate Twitter Message Detected"
-      end
-      
+      end      
     end
   end
   
   def self.get_messages_and_parse(options = Hash.new)
     message_last_updated_at = Message.maximum(:updated_at)
     options[:since] = Message.maximum(:updated_at) unless options[:since] && message_last_updated_at.nil?
+    
+    puts "using options #{options.inspect}"
 
-    Twitter::Base.new(TWITTER_USERNAME,TWITTER_PASSWORD).timeline(:friends).each do |tweet|
+    Twitter::Base.new(TWITTER_USERNAME,TWITTER_PASSWORD).timeline(:friends,options).each do |tweet|
       MessageParser.parse(tweet)      
     end
   end
--- lib/tasks/stream_processing.rake
+++ lib/tasks/stream_processing.rake
@@ -2,13 +2,16 @@ require File.join(File.dirname(__FILE__),'../message_parser')
 
 namespace :stream do
   task :fetch => :environment do
-    last_message = Message.maximum(:updated_at)
     message_count = Message.count
     user_count = Account.count
+    needs_count = Need.count
+    venture_count = Venture.count
     
-    MessageParser.get_messages_and_parse   
+    MessageParser.get_messages_and_parse({:count=>200})   
 
     puts "New Messages: #{Message.count - message_count}"
     puts "New Accounts: #{Account.count - user_count}"
+    puts "New Needs: #{Need.count - needs_count}"
+    puts "New Ventures: #{Venture.count - venture_count}"
   end
 end
