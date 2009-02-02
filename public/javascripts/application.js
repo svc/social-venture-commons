12abaa52d56c34990e8d3e4fb45089192e89ac1b
Dan Williams <dan.williams@itthugs.com>
2009-02-01 22:16:52 -0500

    Added char limit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,4 +19,37 @@ $(document).ajaxSend(function(event, request, settings) {
 
 $(document).ready(function() {
   $(".system-messages").delay(2500).hide("blind", { direction: "vertical" }, 1000);
+	setMaxLength();
 });
+
+function setMaxLength() {
+	var x = document.getElementsByTagName('textarea');
+	var counter = document.createElement('div');
+	counter.className = 'counter';
+	for (var i=0;i<x.length;i++) {
+		if (x[i].getAttribute('maxlength')) {
+			var counterClone = counter.cloneNode(true);
+			counterClone.relatedElement = x[i];
+			counterClone.innerHTML = '<span>0</span>/'+x[i].getAttribute('maxlength');
+			x[i].parentNode.insertBefore(counterClone,x[i].nextSibling);
+			x[i].relatedElement = counterClone.getElementsByTagName('span')[0];
+
+			x[i].onkeyup = x[i].onchange = checkMaxLength;
+			x[i].onkeyup();
+		}
+	}
+}
+
+function checkMaxLength() {
+	var maxLength = this.getAttribute('maxlength');
+	var currentLength = this.value.length;
+	if (currentLength > maxLength) {
+		this.relatedElement.className = 'toomuch';
+		$("#twitter_post").attr("disabled", true); 
+	}	else {
+		this.relatedElement.className = '';
+		$("#twitter_post").attr("disabled", false); 
+	}
+	this.relatedElement.firstChild.nodeValue = currentLength;
+	// not innerHTML
+}
